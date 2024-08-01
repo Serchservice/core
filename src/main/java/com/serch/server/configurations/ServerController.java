94313fc8626ff6f998b9f3ebe42591f3bf02f5fd
Mervo <evaristusadimonyemma@gmail.com>
2024-08-01 13:57:40 +0100

    Modified api.json

--- src/main/java/com/serch/server/configurations/ServerController.java
+++ src/main/java/com/serch/server/configurations/ServerController.java
@@ -3,15 +3,12 @@ package com.serch.server.configurations;
 import com.serch.server.bases.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,16 +31,24 @@ public class ServerController {
     }
 
     @GetMapping("/openapi/save")
-    public ResponseEntity<ApiResponse<String>> saveOpenApiJson() {
+    public ResponseEntity<byte[]> saveOpenApiJson() {
         String openApiUrl = SERCH_BASE_URL + "/v3/api-docs";
         String openApiJson = rest.getForObject(openApiUrl, String.class);
 
-        try (FileOutputStream fos = new FileOutputStream("serch-server-openapi.json")) {
-            assert openApiJson != null;
-            fos.write(openApiJson.getBytes());
-            return ResponseEntity.ok(new ApiResponse<>("OpenAPI JSON file saved successfully!", HttpStatus.OK));
-        } catch (IOException e) {
-            return ResponseEntity.badRequest().body(new ApiResponse<>("Error saving OpenAPI JSON file."));
+        if (openApiJson == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error fetching OpenAPI JSON".getBytes());
         }
+
+        byte[] fileContent = openApiJson.getBytes();
+
+        // Set headers for file download
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setContentDisposition(ContentDisposition.attachment().filename("Serch|ServerApi.json").build());
+        headers.setContentLength(fileContent.length);
+
+        return new ResponseEntity<>(fileContent, headers, HttpStatus.OK);
     }
+
 }

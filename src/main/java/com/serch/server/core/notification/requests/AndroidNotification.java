d4472d558a76241228e180cb89fba2c4bff2ea59
Evaristus Adimonyemma <evaristusadimonyemma@serchservice.com>
2024-08-22 12:21:48 +0000

    Added resource to the notification files

--- src/main/java/com/serch/server/core/notification/requests/AndroidNotification.java
+++ src/main/java/com/serch/server/core/notification/requests/AndroidNotification.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class AndroidNotification {
-    private String icon = "raw/res_favicon_dark";
+    private String icon = "resource://raw/res_favicon_dark";
     private String color = "#050404";
-    private String sound = "raw/res_notify";
+    private String sound = "resource://raw/res_notify";
 }

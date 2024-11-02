2aae5265610f15ca30ddb153988189f36a3eb306
iamevaristus <evaristusadimonyemma@serchservice.com>
2024-11-02 13:01:41 +0100

    Updating fix for Location fieldMask

--- src/main/java/com/serch/server/enums/ServerHeader.java
+++ src/main/java/com/serch/server/enums/ServerHeader.java
@@ -9,7 +9,9 @@ public enum ServerHeader {
     GUEST_API_KEY("X-Serch-Guest-Api-Key"),
     GUEST_SECRET_KEY("X-Serch-Guest-Secret-Key"),
     DRIVE_API_KEY("X-Serch-Drive-Api-Key"),
-    DRIVE_SECRET_KEY("X-Serch-Drive-Secret-Key");
+    DRIVE_SECRET_KEY("X-Serch-Drive-Secret-Key"),
+    GOOGLE_API_KEY("X-Goog-Api-Key"),
+    GOOGLE_FIELD_MASK("X-Goog-FieldMask");
 
     private final String value;
 }

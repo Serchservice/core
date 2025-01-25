3a358b49ed525418a8ec5f3f3ca9010c9a7dea57
iamevaristus <evaristusadimonyemma@serchservice.com>
2025-01-25 12:29:37 +0100

    Updating notification payload and versioning

--- src/main/java/com/serch/server/domains/transaction/services/WalletService.java
+++ src/main/java/com/serch/server/domains/transaction/services/WalletService.java
@@ -1,17 +1,15 @@
 package com.serch.server.domains.transaction.services;
 
 import com.serch.server.bases.ApiResponse;
-import com.serch.server.models.account.Profile;
-import com.serch.server.models.auth.User;
 import com.serch.server.core.payment.responses.InitializePaymentData;
-import com.serch.server.models.transaction.Wallet;
 import com.serch.server.domains.transaction.requests.FundWalletRequest;
 import com.serch.server.domains.transaction.requests.WalletUpdateRequest;
 import com.serch.server.domains.transaction.requests.WithdrawRequest;
-import com.serch.server.domains.transaction.responses.TransactionGroupResponse;
 import com.serch.server.domains.transaction.responses.WalletResponse;
+import com.serch.server.models.account.Profile;
+import com.serch.server.models.auth.User;
+import com.serch.server.models.transaction.Wallet;
 
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -131,28 +129,6 @@ public interface WalletService {
      * @return An ApiResponse indicating the result of the wallet update operation.
      */
     ApiResponse<String> update(WalletUpdateRequest request);
-
-    /**
-     * Fetches the list of transactions for the logged-in user.
-     *<p>
-     * This method retrieves all transactions associated with the user's wallet,
-     * providing a comprehensive overview of their financial activity. The response
-     * includes details about each transaction, such as amounts, dates, and types.
-     *
-     * @return An ApiResponse containing a list of TransactionGroupResponse objects.
-     */
-    ApiResponse<List<TransactionGroupResponse>> transactions(Integer page, Integer size);
-
-    /**
-     * Fetches the recent transactions of the logged-in user.
-     * <p>
-     * This method combines all different transaction types and limits the result to
-     * the most recent five transactions. It allows users to quickly view their latest
-     * financial activities.
-     *
-     * @return An ApiResponse containing a list of TransactionGroupResponse objects.
-     */
-    ApiResponse<List<TransactionGroupResponse>> recent();
 
     /**
      * Processes the payment for Tip2Fix calls at any moment in time.
